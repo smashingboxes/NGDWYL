167510a29478d48d04bb5180db51d68eba00a508
Ian Donovan <idonovan90@gmail.com>
2014-08-29 15:53:13 -0400

    Test the entries#create action and add the show json response

---
+++ test/controllers/entries_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class EntriesControllerTest < ActionController::TestCase
+  
+  # Also needs to be stubbed
+  # test "should create entry" do
+  #   entry_params = { goal: "Travel the whole big wide world!", submitted_by: "@themanthemyththelegendarylegend" }
+  #   assert_difference 'Entry.count', 1 do
+  #     post :create, entry: entry_params
+  #   end
+  #   assert_redirected_to root_url
+  # end
+
+end
--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,4 +1,4 @@
-require_relative "../test_helper"
+require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
   # Gotta find a way to stub this out
