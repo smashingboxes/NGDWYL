b7fb5b8a57d713abc098ef9f9ba94956a817fbb3
Kenneth Ashley <kenneth@smashingboxes.com>
2014-08-29 11:21:34 -0400

    first commit

---
+++ app/assets/javascripts/application.coffee
@@ -0,0 +1,6 @@
+# = require jquery
+# = require jquery_ujs
+# = require turbolinks
+# = require angular
+
+app = angular.module('HomeCtrl', [])
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NGDWYL::Application.config.secret_key_base = 'c64388e9ce98c48a29937462e4e6dfb6d6fdb42fea0ad21df80d430a4b13ec3f6f38c3ae673d256a4fb127cc9ef34700f123ab75fc93f5dc2f6784ad5bea28db'
