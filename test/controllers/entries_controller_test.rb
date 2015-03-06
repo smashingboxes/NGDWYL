8de269c79c383e2ec60f0bd34fac238b2b6a6bd5
Ian Donovan <idonovan90@gmail.com>
2015-03-06 15:25:42 -0500

    Add an API endpoint to return a random entry

--- test/controllers/entries_controller_test.rb
+++ test/controllers/entries_controller_test.rb
@@ -2,13 +2,41 @@ require 'test_helper'
 
 class EntriesControllerTest < ActionController::TestCase
   
-  # Also needs to be stubbed
-  # test "should create entry" do
-  #   entry_params = { goal: "Travel the whole big wide world!", submitted_by: "@themanthemyththelegendarylegend" }
-  #   assert_difference 'Entry.count', 1 do
-  #     post :create, entry: entry_params
-  #   end
-  #   assert_redirected_to root_url
-  # end
+  def test_create_entry
+    assert_difference 'Entry.count', 1 do
+      post :create, entry: attributes_for(:entry)
+    end
+    assert_response(:success)
+  end
+
+  def test_show_entry
+    entry = create(:entry)
+    get :show, id: entry.id, format: :json
+    assert_response(:success)
+    returned_json = JSON.parse(response.body)
+    assert_equal entry.goal, returned_json["goal"]
+    assert_equal entry.submitted_by, returned_json["submitted_by"]
+  end
+
+  def test_index
+    entries = create_list(:entry, 2)
+    get :index, format: :json
+    assert_response(:success)
+    returned_json = JSON.parse(response.body)
+    assert_equal 2, returned_json.size
+    assert_equal JSON.parse(entries.first.to_json), returned_json.first
+    assert_equal JSON.parse(entries.last.to_json), returned_json.last
+  end
+
+  def test_random
+    entries = create_list(:entry, 3)
+    get :random, format: :json
+    assert_response(:success)
+    returned_json = JSON.parse(response.body)
+    jsoned_entries = entries.map do |entry|
+      JSON.parse(entry.to_json)
+    end
+    assert jsoned_entries.include?(returned_json)
+  end
 
 end
